228c0f2a0805bc66b86d407eede7a88c40240504
Navneet <navneetjh5@gmail.com>
2020-10-20 20:24:31 +0530

    ADDED: Searching element 63

--- src/test/java/com/cg/hashtable/MyBinaryTreeTest.java
+++ src/test/java/com/cg/hashtable/MyBinaryTreeTest.java
@@ -36,5 +36,31 @@ public class MyBinaryTreeTest {
 
     }
 
+    public static MyBinaryTree<Integer> myBinaryTree;
+    @Before
+    public void initialize() {
+        myBinaryTree = new MyBinaryTree();
+        myBinaryTree.add(50);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(11);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(65);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+    }
+
+    @Test
+    public void givenNumber_whenSearchInTree_shouldReturnTrue() {
+        boolean present = myBinaryTree.search(63);
+        Assert.assertTrue(present);
+
+    }
+
 
 }
